296d43d73df6cfede604422ad11b7167a20d4a53
GutoSwan <103047445+GutoSwan@users.noreply.github.com>
2023-05-31 17:06:36 -0300

    test

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,47 +1,11 @@
+// Adicione um evento de clique ao botão de abrir a modal
+document.getElementById("openModalBtn").addEventListener("click", function () {
+  // Exiba a modal
+  document.getElementById("navModal").style.display = "block";
+});
 
-
-(function ($) {
-
-  var $window = $(window),
-    $body = $('body');
-
-  // Breakpoints.
-  breakpoints({
-    xlarge: ['1281px', '1680px'],
-    large: ['981px', '1280px'],
-    medium: ['737px', '980px'],
-    small: [null, '736px']
-  });
-
-  // Nav.
-
-  // Title Bar.
-  $(
-    '<div id="titleBar">' +
-    '<a href="#navPanel" class="toggle"></a>' +
-    '<span class="title">' + $('#logo').html() + '</span>' +
-    '</div>'
-  )
-    .appendTo($body);
-
-  // Panel.
-  $(
-    '<div id="navPanel">' +
-    '<nav>' +
-    $('#nav').navList() +
-    '</nav>' +
-    '</div>'
-  )
-    .appendTo($body)
-    .panel({
-      delay: 500,
-      hideOnClick: true,
-      hideOnSwipe: true,
-      resetScroll: true,
-      resetForms: true,
-      side: 'left',
-      target: $body,
-      visibleClass: 'navPanel-visible'
-    });
-
-})(jQuery);
+// Adicione um evento de clique ao botão de fechar a modal
+document.getElementsByClassName("close")[0].addEventListener("click", function () {
+  // Feche a modal
+  document.getElementById("navModal").style.display = "none";
+});
